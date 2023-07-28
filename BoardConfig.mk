b6d4034adeaa362f46a63456670d6875678ea823
johnmart19 <johnivan19999@gmail.com>
2023-07-28 11:59:30 +0300

    zeus: Update Screen Density for QHD Stock resolution
    - Mode: 1440x3200
    - Density 560 (xxxhdpi)
    - PPI 526 (stock)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,4 +19,4 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Screen density
-TARGET_SCREEN_DENSITY := 560
+TARGET_SCREEN_DENSITY := 526
